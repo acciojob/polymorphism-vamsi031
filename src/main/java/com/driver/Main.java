9721c942c77578d330a946c54f9084c4164bf1ef
vamsi <vamsi003311@gmail.com>
2023-06-13 11:53:45 +0530

    polymorpism solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,22 +2,22 @@ package com.driver;
 
 public class Main {
     public static class Product{
-        public int Product(int x,int y){
+        public int product(int x,int y){
             return x*y;
         }
-        public int Product(int x,int y,int z){
+        public int product(int x,int y,int z){
             return x*y*z;
         }
-        public double Product(double x,double y){
+        public double product(double x,double y){
             return x*y;
         }
     }
 
     public static void main(String[] args) {
         Product P = new Product();
-        P.Product(1,2);
-        P.Product(1,2,3);
-        P.Product(2.05,3.02);
+        P.product(1,2);
+        P.product(1,2,3);
+        P.product(2.05,3.02);
     }
 
 }
